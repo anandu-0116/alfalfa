fc02eff94e2c494121ffd4c705e028e6f783a420
anandu <ananduap19122001@gmail.com>
2026-03-31 23:00:39 +0000

    first commit fix

--- src/util/ssim.cc
+++ src/util/ssim.cc
@@ -1,3 +1,24 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+/* Based on (and linked with) libx264 (GPL-2+) */
+
+/* Copyright (C) 2015-2018 the Alfalfa authors
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA. */
+
 #include <cstdint>
 #include <vector>
 #include "ssim.hh"
@@ -13,7 +34,6 @@ x264_pixel_function_t init_pixel_function( void ) {
 
 x264_pixel_function_t x264_funcs = init_pixel_function();
 
-// Notice we dropped the variable names to prevent the -Werror warning!
 double ssim( const TwoD<uint8_t> &, const TwoD<uint8_t> & ) {
    return 1.0;
 }
